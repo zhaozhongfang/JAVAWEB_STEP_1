d0b930d68d4e95d60996b409106eadcedb38d4be
zhaofang <1097981142@qq.com>
2019-10-30 14:47:10 +0800

    11

--- Login/src/csii/com/cn/test/TestConnection.java
+++ Login/src/csii/com/cn/test/TestConnection.java
@@ -14,12 +14,9 @@ public class TestConnection {
         UserDao userDao = new UserDao();
         User testUser = userDao.getConnection(user);
         System.out.println(testUser);
-<<<<<<< HEAD
         System.out.println("345234524244322343");
         System.out.println("3333");
-=======
         System.out.println("12312312");
 
->>>>>>> 798d70b7204f1a663d38eeebddb1ecb23b9f601e
     }
 }

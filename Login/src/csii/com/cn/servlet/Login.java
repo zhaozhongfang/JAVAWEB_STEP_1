12000e49741f575bf5c08e1da6866bafa30669e0
zhaofang <1097981142@qq.com>
2019-11-14 00:10:20 +0800

    122

--- Login/src/csii/com/cn/servlet/Login.java
+++ Login/src/csii/com/cn/servlet/Login.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class Login extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         System.out.println("345234524244322343");
-        private String zhaofang;
+        private String ccc;
         System.out.println("3333");
         System.out.println("3333");
         System.out.println("3333");

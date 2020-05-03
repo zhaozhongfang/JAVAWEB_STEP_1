b54dde53635d86ef6d2eefa2f69c6be8d4d18f10
zhaofang <1097981142@qq.com>
2020-05-03 22:40:57 +0800

    初始化数据

---
+++ Login/src/test.java
@@ -0,0 +1,11 @@
+/**
+ * @program: Login
+ * @description: 测试类
+ * @author: Zhaofang
+ * @create: 22:40 2020-05-03
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("初始化数据");
+    }
+}

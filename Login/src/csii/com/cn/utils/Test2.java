5c5d93a602cedf9f2d7b9a46dafc61c2fc55aeec
zhaofang <1097981142@qq.com>
2020-05-03 22:13:33 +0800

    test2   zhangsan第一次提交

--- Login/src/csii/com/cn/utils/Test2.java
+++ Login/src/csii/com/cn/utils/Test2.java
@@ -10,6 +10,14 @@ public class Test2 {
     public static void main(String[] args) {
         for (int i = 0; i < 12; i++) {
             System.out.println(i);
+            System.out.println(i);
+            System.out.println(i);
+            System.out.println(i);
+            System.out.println(i);
+            System.out.println(i);
+            System.out.println(i);
+            System.out.println(i);
+            System.out.println(i);
         }
     }
 }

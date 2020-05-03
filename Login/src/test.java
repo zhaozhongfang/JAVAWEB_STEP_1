7dd55488d6e0e7159688ae33615930330fed9617
zhaofang <1097981142@qq.com>
2020-05-03 22:44:50 +0800

    Revert "Revert "初始化数据""
    This reverts commit d6701f0e

---
+++ Login/src/test.java
@@ -0,0 +1,13 @@
+/**
+ * @program: Login
+ * @description: 测试类
+ * @author: Zhaofang
+ * @create: 22:40 2020-05-03
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("初始化数据");
+        System.out.println("第二次提交数据");
+        System.out.println("第三次提交数据");
+    }
+}

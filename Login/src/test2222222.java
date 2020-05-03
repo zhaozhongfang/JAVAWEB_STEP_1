4d5725ed8c8ff53bc0566266c1abdc62d360f1c3
zhaofang <1097981142@qq.com>
2020-05-03 23:14:13 +0800

    第三次循环

--- Login/src/test2222222.java
+++ Login/src/test2222222.java
@@ -7,6 +7,9 @@
 public class test2222222 {
     public static void main(String[] args) {
         System.out.println("第一次修改");
+        for (int i = 0; i < 1111; i++) {
+            System.out.println(i);
+        }
         System.out.println("第一次修改");
         System.out.println("第一次修改");
         System.out.println("第一次修改");

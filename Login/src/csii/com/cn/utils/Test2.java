470cf45baa05d8da6a314eabad5a863d5daab07c
zhaofang <1097981142@qq.com>
2020-05-03 22:09:03 +0800

    test2   第一次提交

---
+++ Login/src/csii/com/cn/utils/Test2.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * @program: Login
+ * @description: 测试类2
+ * @author: Zhaofang
+ * @create: 22:08 2020-05-03
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 12; i++) {
+            System.out.println(i);
+        }
+    }
+}

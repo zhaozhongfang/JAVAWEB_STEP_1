e3eb65879e573352fc2d7e60b1f88905e3279540
zhaofang <1097981142@qq.com>
2020-05-03 21:46:54 +0800

    初始化数据

---
+++ Login/src/csii/com/cn/utils/Test.java
@@ -0,0 +1,18 @@
+package utils;
+
+/**
+ * @program: Login
+ * @description: 测试类
+ * @author: Zhaofang
+ * @create: 21:45 2020-05-03
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试分支1");
+        System.out.println("测试分支2");
+        System.out.println("测试分支3");
+        System.out.println("测试分支4");
+        System.out.println("测试分支5");
+        System.out.println("测试分支6");
+    }
+}
